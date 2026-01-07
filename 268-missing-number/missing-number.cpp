191543ea12072f7b41ccfd8e4f545c5363552bc0
Avi <clownalphareal11@gmail.com>
2026-01-07 09:06:45 +0530

    Time: 23 ms (6.24%) | Memory: 28.2 MB (5.06%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        int n=nums.size();
+        for(int i=0 ; i<=n ; i++){
+            mpp[i]=0;
+        }
+        for(int i=0 ; i<n ; i++){
+            mpp[nums[i]]=1;
+        }
+        for(int i=0 ; i<=n ; i++){
+            if(mpp[i]==0){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
