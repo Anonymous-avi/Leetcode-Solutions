e20e202b077a06e6c2b8ef4055f4637075186f29
Avi <clownalphareal11@gmail.com>
2026-01-12 21:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (18.09%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+
+        unordered_map<char, int> mp;
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            mp[ch] = 0;
+        }
+
+        for (char ch : sentence) {
+            mp[ch]++;
+        }
+
+        for (auto it : mp) {
+            if (it.second == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
