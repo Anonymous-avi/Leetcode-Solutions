dfedb5d91e6b90cc5d8a604e9fcaffcc678c4bb3
Avi <clownalphareal11@gmail.com>
2026-07-16 12:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.96%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -3,13 +3,14 @@ public:
     string largestOddNumber(string num) {
        //best way sirf last se ek odd number dekh lo fir woh longest odd substring ban jaayegi
        for(int i=num.size()-1 ; i>=0 ; i--){
-        if((num[i]-'0')%2==1){
-            //i+1 showing the length and 0 se start karke utni length ki substring return kardenge 
-           return  num.substr(0,i+1);
-        }
+        if((num[i]-'0')%2==1)
+        {
+      return num.substr(0,i+1);
+      }      
        }
-       return "";
-
+     //i+1 showing the length and 0 se start karke utni length ki substring return kardenge 
+       
+     return "";
         
     }
 };
