742cae63caed16c4e6d3915218d530922029ff2a
Avi <clownalphareal11@gmail.com>
2026-07-16 13:37:19 +0530

    Time: 3 ms (9.34%) | Memory: 12 MB (39.73%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -6,6 +6,7 @@ public:
        for(int i=0 ; i<strs[0].size() ; i++){
         //saath j ko use karenge baaki strings ke ussi charater number ko check karne ke liye 
         for(int j=1 ; j<strs.size() ; j++){
+            //hum check karenge pehle ki kahi i wala reference charater cross toh nhi kar gaya j waale ko....mwans i ka size j se bada hogaya ...or agar dono characters different hai ...in dono conditions pe ans resturn kar denge 
             if(i>=strs[j].size() || strs[j][i]!=strs[0][i]){
                 return ans;
             }
