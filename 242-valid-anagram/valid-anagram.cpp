12cb370f103987c9028eae7a5e7ac63d66f3abac
Avi <clownalphareal11@gmail.com>
2026-07-18 02:58:25 +0530

    Time: 3 ms (45.96%) | Memory: 9.9 MB (26.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -11,13 +11,13 @@ public:
        }
         for(auto ch : b){
         mppb[ch]++;
-       }
+       }///can compare the maps directly 
        if(mppa==mppb){
         return true;
        }
        return false;
        
-
+    //most optimal yeh hai ki ek hi map banao or pehli string ke liye freq++ karo or dusri ke liye freq--....agar last mei 0 baje toh that means it is anagram
 
         
     }
