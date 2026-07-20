68a99e054a765f15ca1909c86c3482229aa2a068
Avi <clownalphareal11@gmail.com>
2026-07-20 20:06:58 +0530

    Time: 8 ms (82.86%) | Memory: 11.9 MB (23.68%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,18 +9,30 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_set<ListNode*>st;
-        ListNode* temp=head;
-        while(temp!=nullptr){
-            if(st.find(temp)!=st.end()){
-                return true ;
-            }
+        //brute approach but iski TC O(N) hai and SC O(N) therefore it is bad
+      //  unordered_set<ListNode*>st;
+       // ListNode* temp=head;
+       // while(temp!=nullptr){
+       //     if(st.find(temp)!=st.end()){
+        //        return true ;
+        //    }
 
-            st.insert(temp);
-            temp=temp->next;
+        //    st.insert(temp);
+        //    temp=temp->next;
+
+        //}
+        //return false;
+        ListNode* slow=head; //yeh optimal solution hai kyunki ismie koi bhi extra space nhi use kar rhe hum 
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+             
+             if(slow==fast){
+                return true ;
+             }
 
         }
         return false;
-        
     }
 };
