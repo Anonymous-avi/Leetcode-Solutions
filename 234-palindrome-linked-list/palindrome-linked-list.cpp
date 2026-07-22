a45d7bf8de59b9de371e04f2a6a4628c8617bd29
Avi <clownalphareal11@gmail.com>
2026-07-22 10:45:29 +0530

    Time: 0 ms (100.00%) | Memory: 118 MB (82.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,23 +11,59 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-       vector<int>vec;
-       ListNode*temp=head;
-       while(temp!=nullptr){
-        vec.push_back(temp->val);
-        temp=temp->next;
+      //this is brute approach highly easy one ...but isse space complexity increase ho ajayegi
+      // vector<int>vec;
+      // ListNode*temp=head;
+      // while(temp!=nullptr){
+      //  vec.push_back(temp->val);
+       // temp=temp->next;
+       //}
+      // int left=0;
+      // int right=vec.size()-1;
+      // while(left<=right){
+        //if(vec[left]!=vec[right] && left<=right){
+          //  return false;
+        //}
+        //left++;
+        //right--;
+       //}
+
+       //return true;
+
+
+       //OPTIMAL APPROACH 
+       ListNode* slow=head;
+       ListNode* fast=head;
+       while(fast->next!=nullptr && fast->next->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next->next;
+
        }
-       int left=0;
-       int right=vec.size()-1;
-       while(left<=right){
-        if(vec[left]!=vec[right] && left<=right){
+       
+       ListNode*first=head;
+       ListNode*second=reverseMiddle(slow->next);
+       while(second!=nullptr){
+        if(first->val!=second->val){
             return false;
         }
-        left++;
-        right--;
+        first=first->next;
+        second=second->next;
        }
-
        return true;
+
         
     }
+
+private:
+ListNode* reverseMiddle(ListNode*head){
+    ListNode*prev=nullptr;
+    ListNode*current=head;
+    while(current!=nullptr){
+        ListNode*next=current->next;
+        current->next=prev;
+        prev=current;
+        current=next;
+    }
+    return prev;
+}
 };
