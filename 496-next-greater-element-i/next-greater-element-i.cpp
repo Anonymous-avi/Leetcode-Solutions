d0016df149647c0bdb391611be60900e89880c13
Avi <clownalphareal11@gmail.com>
2026-07-30 09:17:39 +0530

    Time: 11 ms (5.85%) | Memory: 12.2 MB (92.70%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        vector<int>ans;
-        for(int i=0 ; i<n ; i++){
-            bool found =false;
-            for(int j=0 ; j<m ; j++){
-                if(nums1[i]==nums2[j]){
-                    for(int k=j ; k<m ; k++){
-                        if(nums2[k]>nums2[j]){
-                            ans.push_back(nums2[k]);
-                            found=true;
-                            break;
-                        }
-                        
-                    }
-                    if(found==false){
-                        ans.push_back(-1);
-                    }
-                    break;
-                    
-                }
-                
+       int temp=0;
+       vector<int>ans;
+       for(int i=0 ; i<nums1.size() ; i++){
+        for(int j=0 ; j<nums2.size() ; j++){
+            if(nums1[i]==nums2[j]){
+                temp=j;
             }
         }
-        return ans;
+
+        int nextGreater=-1;
+        for(int j=temp+1; j<nums2.size() ; j++){
+            if(nums2[j]>nums2[temp]){
+                nextGreater=nums2[j];
+                break;
+            }
+
+        }
+        ans.push_back(nextGreater);
+       }
+
+       return ans;
+       
         
     }
 };
