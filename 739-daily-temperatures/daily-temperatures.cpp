c791838ee23a35bc4d76f15d15838a1f50eaa6bc
Avi <clownalphareal11@gmail.com>
2026-08-01 09:14:58 +0530

    Time: 27 ms (33.93%) | Memory: 108.3 MB (10.38%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -27,25 +27,26 @@ public:
         ans.push_back(0);
         return ans; */
 
-      int n = temperatures.size();
-        vector<int> ans(n, 0);
-
-        stack<int> st;   // to store indices
-
-        for(int i = n - 1; i >= 0; i--) {
-
-            while(!st.empty() && temperatures[st.top()] <= temperatures[i]) {
-                st.pop();
-            }
-
-            if(!st.empty()) {
-                ans[i] = st.top() - i;
-            }
-
-            st.push(i);
+      stack<int>st;
+      vector<int>ans;
+      for(int i=temperatures.size()-1 ; i>=0 ; i--){
+        
+        while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
+            st.pop();
+            
         }
+        if(st.empty()){
+            ans.push_back(0);
+        }
+        else{
+            ans.push_back(st.top()-i);
+        }
+        st.push(i);
+      }
 
-        return ans;    
+      reverse(ans.begin() , ans.end());
+
+      return ans;
     }
 
 };
