9efe3743e921787937bf141c22acc62f63f3167d
Avi <clownalphareal11@gmail.com>
2026-08-07 01:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (16.41%) - LeetSync

---
+++ 3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        
+        for(int i=0 ; i<s.size() ; i++){
+            s[i]=tolower(s[i]);
+        }
+        int count=0;
+        for(int i=0 ; i<s.size()-1 ; i++){
+            if(s[i+1]!=s[i]){
+                count++;
+            }
+            
+        }
+
+        return count;
+
+    }
+};
