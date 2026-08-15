a8127b86d6d6b25640f0387a25ea172cdecc37e7
Avi <clownalphareal11@gmail.com>
2026-08-15 10:02:30 +0530

    Time: 15 ms (69.23%) | Memory: 27.4 MB (20.76%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mpp;
+        vector<vector<string>>ans;
+        for(auto x : strs){
+            string temp=x;
+            sort(x.begin() , x.end());
+            mpp[x].push_back(temp);
+        }
+        for(auto x : mpp){
+            vector<string>temp=x.second;
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
