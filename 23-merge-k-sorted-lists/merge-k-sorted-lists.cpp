2cf4e7654cc2aafeb6b03ff0ceca7c30b64a535e
Avi <clownalphareal11@gmail.com>
2026-08-18 10:56:13 +0530

    Time: 2 ms (67.33%) | Memory: 19.2 MB (9.26%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        //brute approach is ki saari lists ko ek array mei daal dein...fir usse sort kar denge ...and then finally usse sorted array ko linked list mei convert kar denge 
+
+        vector<int>res;
+        for(int i=0 ; i<lists.size() ; i++){
+            ListNode*temp=lists[i];
+
+            while(temp!=nullptr){
+                res.push_back(temp->val);
+                temp=temp->next;
+            }
+        }
+        sort(res.begin(),res.end());
+
+        //convert this to ll
+        ListNode* dummy=new ListNode(-1);
+        ListNode* tail=dummy;
+
+        for(int i=0 ; i<res.size() ; i++){
+            tail->next = new ListNode(res[i]);
+            tail=tail->next;
+        }
+
+        return dummy->next; 
+
+
+    }
+};
