4c74921b31f7dec7b57c20a12173ec97e43550f8
Avi <clownalphareal11@gmail.com>
2026-08-22 09:55:26 +0530

    Time: 4 ms (42.26%) | Memory: 19 MB (19.99%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -13,7 +13,7 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         //brute approach is ki saari lists ko ek array mei daal dein...fir usse sort kar denge ...and then finally usse sorted array ko linked list mei convert kar denge 
 
-       vector<int>res;
+       /*vector<int>res;
        //step 1 : pehle list ko vector mei add kiya 
        for(int i=0 ; i<lists.size() ; i++){
         ListNode* temp=lists[i];
@@ -26,7 +26,7 @@ public:
        sort(res.begin(),res.end());
 
        //step 3 : vector ko linked list mei conevrt kardo
-       if(res.empty()){
+       if(res.empty()){  //base case is mandatory
         return {};
        }
        ListNode*head=new ListNode(res[0]);
@@ -37,8 +37,34 @@ public:
         temp=temp->next;
        }
 
-       return head;
+       return head;*/
 
+    //optimal approach of w3hat I can think of is ki we can use a min heap so that sorting time complexity will be reduced 
+
+    priority_queue<int,vector<int>,greater<int>>pq;
+    for(int i=0 ; i<lists.size() ; i++){
+        ListNode* temp=lists[i];
+        while(temp!=nullptr){
+            pq.push(temp->val);
+            temp=temp->next;
+        }
+    }
+
+    if(pq.empty()){   //base case is mandatory
+        return nullptr;
+    }
+    ListNode*head=new ListNode(pq.top()); //we cannot use pq.front() in a priority_queue we need to use pq.top only to refer to the top most element coz it is like a treee not actually a queue
+    pq.pop();
+    ListNode*temp=head;
+    while(!pq.empty()){
+        ListNode*newNode=new ListNode(pq.top());
+        pq.pop();
+        temp->next=newNode;
+        temp=temp->next;
+    }
+
+
+    return head;
 
 
 
