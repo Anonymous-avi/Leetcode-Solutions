6d05e3a22d867f68962cd1034d77a66178c2571b
Avi <clownalphareal11@gmail.com>
2026-08-24 17:59:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (29.34%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n=s.length();
+        vector<int>freq(26,0);
+
+        for(auto &ch : s){
+            freq[ch-'a']++; 
+
+            //check karte hue chalna hai ki kisi ki bhi freq (n+1)/2 se badi hai ya nhi therefore 
+
+            if(freq[ch-'a']>(n+1)/2){
+                return "";
+            }
+        }
+        priority_queue<pair<int,char>>pq;
+        for(int i = 0; i < 26; i++){
+    if(freq[i] > 0){
+        pq.push({freq[i], 'a' + i});
+    }
+}
+        string result="";
+        while(pq.size()>=2){
+            auto p1=pq.top();
+            pq.pop();
+            auto p2=pq.top();
+            pq.pop();
+
+            result.push_back(p1.second);
+            p1.first--;
+            result.push_back(p2.second);
+            p2.first--;
+
+            if(p1.first>0){
+                pq.push(p1);
+            }
+            if(p2.first>0){
+                pq.push(p2);
+            }
+        }
+        if(!pq.empty()){
+            result.push_back(pq.top().second);
+        }
+        return result;
+    }
+};
