8a2b86b3d49f28810c2165984c6ad1bc9c18ba6e
Avi <clownalphareal11@gmail.com>
2026-09-14 11:15:40 +0530

    Time: 2 ms (39.40%) | Memory: 11.7 MB (14.79%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    public:
+    void solve(vector<int>&nums,int i , vector<int>&temp , vector<vector<int>>&result){
+        sort(nums.begin() , nums.end());
+        
+        //base case
+        if(i>=nums.size()){
+            result.push_back(temp);
+            return;
+        }
+        //Case 1 : to include the current element in the subset 
+        temp.push_back(nums[i]);
+        //then explore furthur
+        solve(nums,i+1,temp,result);
+        //Case 2 : to not include the current element
+        temp.pop_back();
+        //explore futhur
+        solve(nums,i+1,temp,result);
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+       vector<vector<int>>result;
+       vector<vector<int>>ans;
+       vector<int>temp;
+       int i=0;
+       solve(nums,i,temp,result);
+       
+       sort(result.begin() , result.end());
+       ans.push_back(result[0]);
+       for(int i=1 ; i<result.size() ; i++){
+        if(result[i]!=result[i-1]){
+            ans.push_back(result[i]);
+
+        }
+       }
+
+       return ans;
+        
+    }
+};
